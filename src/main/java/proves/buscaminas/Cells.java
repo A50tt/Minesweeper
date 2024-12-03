349dc81eae93537c5c2456c2743e7608bb9f9763
A50tt <alexsarraolmo@gmail.com>
2024-12-03 11:53:03 +0100

    - Added new constructors: by passing string and by passing int[][] - Added method assignNumbers() inside Cells class

--- src/main/java/proves/buscaminas/Cells.java
+++ src/main/java/proves/buscaminas/Cells.java
@@ -1,21 +1,28 @@
-
 package proves.buscaminas;
 
 public class Cells {
-    
+
     int[][] cells;
     int bombs;
-    
+
     public int[][] getCells() {
         return cells;
     }
-    
+
+    public void setBombs(int _bombs) {
+        this.bombs = _bombs;
+    }
+
+    public void addBomb() {
+        this.bombs++;
+    }
+
     public Cells(int x, int y, int _bombs) {
         this.bombs = _bombs;
         this.cells = CellsUtils.assignBombs(x, y, bombs);
         this.cells = CellsUtils.assignNumbers(this.cells);
     }
-    
+
     public Cells(int[][] _cells) {
         this.cells = _cells;
         this.bombs = 0;
@@ -27,16 +34,83 @@ public class Cells {
             }
         }
     }
-    
+
+    public Cells(String cellStr) {
+        int cellOpenings = 0, indexCellOpening = 0;
+        while (indexCellOpening != -1) {
+            indexCellOpening = cellStr.indexOf("[", indexCellOpening);
+            if (indexCellOpening != -1) {
+                cellOpenings++;
+                indexCellOpening++;
+            }
+        }
+
+        int newLines = 0, indexNewLine = 0;
+        while (indexNewLine != -1) {
+            indexNewLine = cellStr.indexOf('\n', indexNewLine);
+            if (indexNewLine != -1) {
+                newLines++;
+                indexNewLine++;
+            }
+        }
+
+        cells = new int[cellOpenings / newLines][newLines];
+        this.bombs = 0;
+        int x = 0, y = 0;
+        boolean bombFound = false;
+        for (char ch : cellStr.toCharArray()) {
+            switch (ch) {
+                case '[':
+                case ']':
+                case '\t':
+                    break;
+                case '-':
+                    addBomb();
+                    cells[x][y] = -1;
+                    bombFound = true;
+                    break;
+                case '\n':
+                    x++;
+                    y = 0;
+                    break;
+                case '1':
+                    if (bombFound) {
+                        bombFound = false;
+                        y++;
+                        break;
+                    }
+                default:
+                    cells[x][y] = Integer.valueOf(Character.toString(ch));
+                    y++;
+                    break;
+            }
+        }
+    }
+
     @Override
     public String toString() {
         String str = "";
         for (int[] row : cells) {
-            for (int cell: row) {
+            for (int cell : row) {
                 str += "[" + cell + "]\t";
             }
             str += "\n";
         }
         return str;
+    }
+
+    public void assignNumbers() {
+        int x, y = 0;
+        for (int[] row : this.cells) {
+            x = 0;
+            for (int cell : row) {
+                if (cell != -1 && cell != 0) {
+                    this.cells[y][x] = 0;
+                }
+                x++;
+            }
+            y++;
+        }
+        CellsUtils.assignNumbers(this.cells);
     }
 }
